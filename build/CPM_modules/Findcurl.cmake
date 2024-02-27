333baa54eb32f6aadff846d1d8cfad0765173f3d
Alexander Tsyrenov <tsyrenov-alexander@matchmaker-team.ru>
2024-02-27 15:31:58 +0300

    fix

--- build/CPM_modules/Findcurl.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/shindaq/mirea/projects/http-client/build/cmake/CPM_0.38.7.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;curl/curl;GIT_TAG;curl-8_5_0;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(curl_FOUND TRUE)
--- build/CPM_modules/Findjson.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/shindaq/mirea/projects/http-client/build/cmake/CPM_0.38.7.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;nlohmann/json;VERSION;3.11.3;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(json_FOUND TRUE)
